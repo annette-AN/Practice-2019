0f91c34e06180058c5cacb679faae8c8c053e4ac
miryean <mirye.ann@gmail.com>
2019-05-23 17:01:46 +0900

    gnb-header position변경, tag줄이기

--- Jungle-Academy/portfolio4-admin_design/js/index.js
+++ Jungle-Academy/portfolio4-admin_design/js/index.js
@@ -1,12 +1,12 @@
+false && $('a').on('click', function(e){
+  e.preventDefault();
+});
+
 $('.user-info > li > a').on('click', function(){
   $('.user-info > li > ul').toggleClass('on');
 });
 
-false && $('#main-gnb-menu > li > a').on('click', function(){
+$('#main-gnb-menu > li > a').on('click', function(){
+  
 
-  var subMenu = $('#main-gnb-menu > li > ul');
-
-  if($(this).next() === subMenu) {
-    subMenu.toggleClass('show');
-  };
 });
