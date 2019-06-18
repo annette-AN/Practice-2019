997390243a78cd49d68c4a1e398b3ef85a6d4dd6
안미례 <mirye.ann@gmail.com>
2019-06-19 00:24:40 +0900

    comit

--- Jungle-Academy/portfolio5-full_page_design/js/Untitled-1.js
+++ Jungle-Academy/portfolio5-full_page_design/js/Untitled-1.js
@@ -1,21 +1,4 @@
 //아래 펑션이 도큐먼트 레디와 같은 의미
-$(function() {
-    new fullpage('#fullpage', {
-        navigation: true,
-        scrollOverflow: true,
-        anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
-        afterLoad: function(origin, destination, direction) {
-            var loadedSection = this;
-
-            if(origin.anchor == 'thirdPage' && direction == 'down') {
-                $('.next-btn').css({display: 'none'});
-            } else {
-                $('.next-btn').css({display: 'inline'});
-            }
-        }
-    });
-});
-
 $(document).on('click', 'a[href="#"]', function(e){
     e.preventDefault();
 });
